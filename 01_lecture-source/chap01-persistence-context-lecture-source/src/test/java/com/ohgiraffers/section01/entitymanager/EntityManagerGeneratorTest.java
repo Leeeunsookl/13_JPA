9dd60184c9d6e089994cb6549ca982995297e910
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-06-04 14:59:14 +0900

    [commit]

--- 01_lecture-source/chap01-persistence-context-lecture-source/src/test/java/com/ohgiraffers/section01/entitymanager/EntityManagerGeneratorTest.java
+++ 01_lecture-source/chap01-persistence-context-lecture-source/src/test/java/com/ohgiraffers/section01/entitymanager/EntityManagerGeneratorTest.java
@@ -1,9 +1,8 @@
 package com.ohgiraffers.section01.entitymanager;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.*;
 
 public class EntityManagerGeneratorTest {
 
@@ -39,5 +38,68 @@ public class EntityManagerGeneratorTest {
     *   따라서 데이터베이스를 사용하는 어플리케이션 당 한 개의
     *   EntityManagerFactory 를 생성한다.
     *  */
+
+    @Test
+    @DisplayName("엔티티 매니저 팩토리 생성 확인하기")
+    void testCreateFactory() {
+
+        EntityManagerFactory factory = EntityManagerFactoryGenerator.getInstance();
+
+        System.out.println("엔티티 매니저 팩토리 hashcode : " + factory.hashCode());
+
+        Assertions.assertNotNull(factory);
+
+    }
+
+    @Test
+    @DisplayName("엔티티 매니저 팩토리 싱글톤 인스턴스 확인")
+    void testFactoryIsSingle() {
+
+        //given
+        EntityManagerFactory factory1 = EntityManagerFactoryGenerator.getInstance();
+        //when
+        EntityManagerFactory factory2 = EntityManagerFactoryGenerator.getInstance();
+        //then
+        System.out.println("factory1 해쉬코드 : " + factory1.hashCode());
+        System.out.println("factory2 해쉬코드 : " + factory2.hashCode());
+
+        Assertions.assertEquals(factory1, factory2);
+
+    }
+
+    /* 필기.
+    *   엔티티 매니저
+    *   엔티티 매니저는 엔티티를 저장하는 메모리 상의 데이터베이스를 관리하는 인스턴스이다.
+    *   엔티티를 저장하고, 수정, 삭제, 조회하는 등의 엔티티와 관련 된 모든 일을 한다.
+    *   엔티티 매니저는 팩토리와 달리 thread-safe 하지 않기 때문에 동시성 문제가 발생할 수 있다.
+    *   따라서 web 의 경우 일반적으로 request-scope 와 일치 시킨다.
+    *  */
+
+    @Test
+    @DisplayName("엔티티 매니저 생성 확인")
+    void testCreateManager() {
+
+        EntityManager manager = EntityManagerGenerator.getInstance();
+
+        System.out.println("manager 의 해시코드 : " + manager.hashCode());
+
+        Assertions.assertNotNull(manager);
+    }
+
+    @Test
+    @DisplayName("엔티티 매니저 스코프 확인")
+    void testManagerLifeCycle() {
+
+        //given
+        EntityManager manager1 = EntityManagerGenerator.getInstance();
+        //when
+        EntityManager manager2 = EntityManagerGenerator.getInstance();
+        //then
+        System.out.println("manager1.hashCode() = " + manager1.hashCode());
+        System.out.println("manager2.hashCode() = " + manager2.hashCode());
+
+        Assertions.assertNotEquals(manager1, manager2);
+
+    }
 
 }
