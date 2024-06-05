3c907879b84a7ff9d69ff8a1886cb8c660ea4987
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-06-05 14:59:38 +0900

    commit

--- 01_lecture-source/chap01-persistence-context-lecture-source/src/test/java/com/ohgiraffers/section03/entity/EntityLifeCycleTests.java
+++ 01_lecture-source/chap01-persistence-context-lecture-source/src/test/java/com/ohgiraffers/section03/entity/EntityLifeCycleTests.java
@@ -1,10 +1,12 @@
 package com.ohgiraffers.section03.entity;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class EntityLifeCycleTests {
@@ -58,6 +60,63 @@ public class EntityLifeCycleTests {
         Menu menu2 = manager.find(Menu.class, menuCode);
 
         Assertions.assertEquals(menu1, menu2);
+
+    }
+
+    @DisplayName("준영속화 detach 테스트")
+    @ParameterizedTest
+    @CsvSource({"11, 1000", "12, 1000"})
+    void testDetachEntity(int menuCode, int menuPrice) {
+
+        EntityManager manager = EntityManagerGenerator.getManagerInstance();
+        EntityTransaction transaction = manager.getTransaction();
+
+        Menu foundMenu = manager.find(Menu.class, menuCode);
+
+        transaction.begin();
+
+        /* detach()
+        *   특정 엔티티만 준영속 상태(영속성 컨텍스트가 관리하던 엔티티 객체를 관리하지 않는 상태)로 만든다.
+        *  */
+
+        manager.detach(foundMenu);
+        foundMenu.setMenuPrice(menuPrice);
+
+        manager.flush();
+
+        Assertions.assertEquals(menuPrice, manager.find(Menu.class, menuCode).getMenuPrice());
+        transaction.rollback();
+
+    }
+
+    @DisplayName("준영속화 detach 후 다시 영속화 테스트")
+    @ParameterizedTest(name = "[{index}] 준영속화 된 {0} 번 메뉴를 {1}원으로 변경하고 다시 영속화 되는 지 확인")
+    @CsvSource({"11, 1000", "12, 1000"})
+    void testDetachAndPersist(int menuCode, int menuPrice) {
+
+        EntityManager manager = EntityManagerGenerator.getManagerInstance();
+        EntityTransaction transaction = manager.getTransaction();
+
+        Menu foundMenu = manager.find(Menu.class, menuCode);
+
+        transaction.begin();
+        manager.detach(foundMenu);
+        foundMenu.setMenuPrice(menuPrice);
+
+        /* 필기.
+        *   파라미터로 넘어온 준영속 엔티티 객체의 식별자 값으로 1차 캐시에서 엔티티 객체를
+        *   조회한다.
+        *   만약 1차 캐시에 엔티티가 없으면 데이터베이스에서 엔티티를 조회하고 1차 캐시에
+        *   저장한다.
+        *   조회한 영속 엔티티 객체에 준영속 상태의 엔티티 객체의 값을 병합한 뒤
+        *   영속 엔티티 객체를 반환한다.
+        *   혹은 조회할 수 없는 데이터의 경우 새로 생성해서 병합한다.(save or update)
+        * */
+        manager.merge(foundMenu);
+        manager.flush();
+
+        Assertions.assertEquals(menuPrice, manager.find(Menu.class, menuCode).getMenuPrice());
+        transaction.rollback();
 
     }
 
