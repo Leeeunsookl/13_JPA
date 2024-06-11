89f3b093dbfda4b7628e7daf4be314acf431de65
DESKTOP-E93NKT3\hi-sinchon <dmstnr738@gmail.com>
2024-06-11 17:55:48 +0900

    추가

---
+++ 01_lecture-source/chap04-jpql-lecture-source/src/main/java/com/ohgiraffers/jpql/section01/simple/SimpleJPQLRepository.java
@@ -0,0 +1,81 @@
+package com.ohgiraffers.jpql.section01.simple;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class SimpleJPQLRepository {
+
+    @Autowired
+    private EntityManager manager;
+
+
+    public String selectSingleMenuByTypedQuery() {
+
+        String jpql = "SELECT m.menuName FROM section01Menu m WHERE m.menuCode = 8";
+        TypedQuery<String> query = manager.createQuery(jpql, String.class);
+        String resultMenuName = query.getSingleResult();
+
+        return resultMenuName;
+
+    }
+
+    public Menu findMenu(int menuCode) {
+
+        return manager.find(Menu.class, menuCode);
+
+    }
+
+    public Object selectSingleMenuByQuery() {
+        String jpql = "SELECT m.menuName FROM section01Menu m WHERE m.menuCode = 8";
+        Query query = manager.createQuery(jpql);
+        Object resultMenuName = query.getSingleResult();
+
+        return  resultMenuName;
+    }
+
+    public Menu selectSingleRowByTypedQuery() {
+
+        String jpql = "SELECT m FROM section01Menu m WHERE m.menuCode=8";
+        //m-> 엔티티의 별칭을 넣은것 section01Menu m
+        TypedQuery<Menu> query = manager.createQuery(jpql, Menu.class);
+        Menu resultMenu = query.getSingleResult();
+
+        return resultMenu;
+
+    }
+
+    public List<Menu> selectMultiRowByTypedQuery() {
+
+        String jpql = "SELECT m FROM section01Menu m";
+        TypedQuery<Menu> query = manager.createQuery(jpql, Menu.class);
+        List<Menu> resultMenuList = query.getResultList();
+
+        return resultMenuList;
+    }
+
+    public List<Menu> selectMultiRowByQuery() {
+
+        String jpql = "SELECT m FROM section01Menu m";
+        Query query = manager.createQuery(jpql);
+        //반환값 지정안했으니 Menu.class 지정x
+        List<Menu> resultMenuList = query.getResultList();
+
+        return resultMenuList;
+    }
+
+    public List<Integer> selectUseDistinct() {
+
+        String jpql = "SELECT DISTINCT m.categoryCode FROM section01Menu m";
+        TypedQuery<Integer> query = manager.createQuery(jpql, Integer.class);
+        List<Integer> resultCategoryCodeList = query.getResultList();
+
+        return resultCategoryCodeList;
+
+    }
+}
