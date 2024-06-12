47f786d676cd13d167d5ea6050bc732d4f61e0f3
DESKTOP-E93NKT3\hi-sinchon <dmstnr738@gmail.com>
2024-06-12 12:30:42 +0900

    추가

---
+++ 01_lecture-source/chap04-jpql-lecture-source/src/main/java/com/ohgiraffers/jpql/section06/projection/ProjectionService.java
@@ -0,0 +1,34 @@
+package com.ohgiraffers.jpql.section06.projection;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ProjectionService {
+
+    private ProjectionRepository repository;
+
+    public ProjectionService(ProjectionRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    public List<Menu> singleEntityTest() {
+
+        List<Menu> menuList = repository.singleEntityTest();
+
+        Menu menu = repository.findMenu(1);
+        System.out.println("menu : " + menu);
+
+        menuList.get(7).setMenuName("새로운 이름");
+
+        return menuList;
+
+    }
+
+    public BiDirectionCategory biDrectionProjection(int menuCode) {
+        return null;
+    }
+}
